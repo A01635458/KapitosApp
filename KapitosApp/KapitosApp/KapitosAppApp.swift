1a0195b1a443abbd069f24805b61eb6338facd8a
A01635458 <A01635458@tec.mx>
2025-11-15 11:37:28 -0600

    first commit

---
+++ KapitosApp/KapitosApp/KapitosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KapitosAppApp.swift
+//  KapitosApp
+//
+//  Created by Luisa Cardona on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KapitosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
